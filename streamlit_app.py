db44e9f12bfa62e77766d79df85fb9a53df83277
Geethanjali R <115665199+Geethanjaliraju@users.noreply.github.com>
2022-10-13 13:57:26 +0530

    Update streamlit_app.py
    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥭 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍌 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🍇 Hard-Boiled Free-Range Egg')
-streamlit.text('🥝 Avocado Toast'
+streamlit.text('🥝 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
